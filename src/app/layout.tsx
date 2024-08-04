5501fa2d662f42273289da03b2afd72f84b27cf4
Arcandres <arcandres@hotmail.com>
2024-08-04 18:24:56 -0500

    Font work around (pending...)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from 'next';
-import { Ubuntu } from 'next/font/google';
 import './globals.css';
-
-const ubuntu = Ubuntu({ subsets: ['latin'], weight: ['300', '500', '700'] });
 
 export const metadata: Metadata = {
   title: 'Blogger Reader',
@@ -16,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={ubuntu.className}>
+      <body>
         <div className='container max-w-2xl mx-auto min-h-[100vh]'>
           {children}
         </div>
