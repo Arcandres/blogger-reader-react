edd953ac227a952a805981d89f0658df77b809b2
Arcandres <arcandres@hotmail.com>
2024-08-04 18:13:01 -0500

    Blog component

---
+++ src/app/components/blog-view.tsx
@@ -0,0 +1,33 @@
+import { Blog } from '@prisma/client';
+import Link from 'next/link';
+import BlogViewTitle from '@/app/components/blog-view-title';
+
+type BlogViewProps = {
+  blog: Blog;
+};
+
+export default function BlogView({ blog }: BlogViewProps) {
+  return (
+    <article className='flex gap-y-4 flex-col w-full max-w-[460px] bg-post p-4 rounded-lg mx-auto'>
+      <BlogViewTitle blogTitle={blog.name} blogUrl={blog.url} />
+      <p className='text-white/80'>{blog.description}</p>
+      <div className='flex justify-between'>
+        <small className='flex gap-x-3'>
+          <span>
+            Posts: <span className='font-bold'>{blog.postsCount}</span>
+          </span>
+          |
+          <span>
+            Pages: <span className='font-bold'>{blog.pagesCount}</span>
+          </span>
+        </small>
+        <Link
+          href={`/blog/${blog.id}`}
+          className='text-xs text-white font-bold hover:text-background-radial transition-colors duration-100'
+        >
+          Read
+        </Link>
+      </div>
+    </article>
+  );
+}
