54f36f1cf11e5adc270265738e757903809f6e45
Arcandres <arcandres@hotmail.com>
2024-08-04 18:12:15 -0500

    Individual post

---
+++ src/app/posts/[postId]/page.tsx
@@ -0,0 +1,30 @@
+import Header from '@/app/components/header';
+import { getPost } from '../../lib/bloggerService';
+
+type PostParams = {
+  searchParams: {
+    blogId: string;
+  };
+  params: {
+    postId: string;
+  };
+};
+
+export default async function page({ searchParams, params }: PostParams) {
+  const postId = params.postId;
+  const blogId = searchParams.blogId;
+
+  const { data: post } = await getPost(blogId, postId);
+
+  if (!post) return 'Post not found';
+
+  return (
+    <main>
+      <Header>{post.title}</Header>
+      <article
+        className='post bg-main rounded-md p-4 m-4'
+        dangerouslySetInnerHTML={{ __html: post.content }}
+      />
+    </main>
+  );
+}
