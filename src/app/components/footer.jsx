6ba415c46790113066344ccad3cb682dc5abe930
Arcandres <arcandres@hotmail.com>
2024-08-05 13:34:15 -0500

    Footer width fixed

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className='fixed bottom-0 flex justify-center w-full bg-main border-t border-teal-100/20 p-2 z-10'>
+    <footer className='fixed bottom-0 flex justify-center w-full max-w-2xl bg-main border-t border-teal-100/20 p-2 z-10'>
       <Link href={'/'}>
-        <HomeIcon className='' />
+        <HomeIcon />
       </Link>
     </footer>
   );
