87ed672e756cf09f6db91246cf46a530de829cff
Arcandres <arcandres@hotmail.com>
2024-08-17 12:25:00 -0500

    Better error handling

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,36 +3,38 @@
 import prisma from '@/app/lib/db';
 import { getBlog } from '@/app/lib/bloggerService';
 import { revalidatePath } from 'next/cache';
-import { notFound, redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export async function createBlog(formData: FormData) {
   if (!formData.get('BlogUrl')) return;
 
-  const { data } = await getBlog(String(formData.get('BlogUrl')));
+  try {
+    const { data } = await getBlog(String(formData.get('BlogUrl')));
 
-  if (!data) return notFound();
+    const restructuredData = {
+      kind: data.kind,
+      id: data.id,
+      name: data.name,
+      description: data.description,
+      published: new Date(data.published),
+      updated: new Date(data.updated),
+      url: data.url,
+      selfLink: data.selfLink,
+      postsCount: data.posts.totalItems,
+      postsUrl: data.posts.selfLink,
+      pagesCount: data.pages.totalItems,
+      pagesUrl: data.pages.selfLink,
+      language: data.locale.language,
+      country: data.locale.country,
+    };
 
-  const restructuredData = {
-    kind: data.kind,
-    id: data.id,
-    name: data.name,
-    description: data.description,
-    published: new Date(data.published),
-    updated: new Date(data.updated),
-    url: data.url,
-    selfLink: data.selfLink,
-    postsCount: data.posts.totalItems,
-    postsUrl: data.posts.selfLink,
-    pagesCount: data.pages.totalItems,
-    pagesUrl: data.pages.selfLink,
-    language: data.locale.language,
-    country: data.locale.country,
-  };
+    await prisma.blog.create({
+      data: restructuredData,
+    });
 
-  await prisma.blog.create({
-    data: restructuredData,
-  });
-
-  revalidatePath(`/blogs`);
-  redirect(`/blogs`);
+    revalidatePath(`/blogs`);
+    redirect(`/blogs`);
+  } catch {
+    console.log('Blog not found or already added');
+  }
 }
