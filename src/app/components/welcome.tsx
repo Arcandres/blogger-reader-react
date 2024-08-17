73f9b2a7e2f19bdccf80099800a06ad398894a3e
Arcandres <arcandres@hotmail.com>
2024-08-17 12:42:46 -0500

    Blogger logo into svg element and removing files

--- src/app/components/welcome.tsx
+++ src/app/components/welcome.tsx
@@ -1,18 +1,11 @@
-import Image from 'next/image';
 import BlogForm from './blog-form';
+import BloggerLogo from './blogger-logo';
 
 export default async function Welcome() {
   return (
     <main className='flex flex-col justify-center items-center  min-h-[100dvh]'>
       <header className='flex flex-col gap-4 items-center'>
-        <Image
-          src='Blogger-logo.svg'
-          alt='Blogger Reader Logo'
-          width={80}
-          height={80}
-          priority={true}
-          className='rounded-2xl bg-orange-300'
-        />
+        <BloggerLogo />
         <h1 className='text-3xl'>Blogger Reader</h1>
         <p>A simple app for reading blogspot Blogs.</p>
       </header>
