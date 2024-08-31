5034d1dfa5797aff2695cd863450ffed2e441c13
Arcandres <arcandres@hotmail.com>
2024-08-31 15:39:20 -0500

    Using normal fetch instead of axios

--- src/app/lib/bloggerService.ts
+++ src/app/lib/bloggerService.ts
@@ -1,25 +1,32 @@
 'use server';
 
-import axios from 'axios';
 import { TBlog, TPosts, TSinglePost } from './types';
 
-const bloggerService = axios.create({
-  baseURL: 'https://www.googleapis.com/blogger/v3',
-});
+const bloggerService = 'https://www.googleapis.com/blogger/v3';
 
 export const getBlog = async (blogUrl: string): Promise<TBlog> =>
-  bloggerService.get(`/blogs/byurl?url=${blogUrl}&key=${process.env.API_KEY}`);
+  fetch(
+    `${bloggerService}/blogs/byurl?url=${blogUrl}&key=${process.env.API_KEY}`
+  )
+    .then((data) => data.json())
+    .then((blog) => ({ data: blog }));
 
 export const getBlogById = async (blogId: string): Promise<TBlog> =>
-  bloggerService.get(`/blogs/${blogId}?key=${process.env.API_KEY}`);
+  fetch(`${bloggerService}/blogs/${blogId}?key=${process.env.API_KEY}`)
+    .then((data) => data.json())
+    .then((blog) => ({ data: blog }));
 
 export const getPosts = async (blogID: string): Promise<TPosts> =>
-  bloggerService.get(`/blogs/${blogID}/posts?key=${process.env.API_KEY}`);
+  fetch(`${bloggerService}/blogs/${blogID}/posts?key=${process.env.API_KEY}`)
+    .then((data) => data.json())
+    .then((blog) => ({ data: blog }));
 
 export const getPost = async (
   blogId: string,
   postId: string
 ): Promise<TSinglePost> =>
-  bloggerService.get(
-    `/blogs/${blogId}/posts/${postId}?key=${process.env.API_KEY}`
-  );
+  fetch(
+    `${bloggerService}/blogs/${blogId}/posts/${postId}?key=${process.env.API_KEY}`
+  )
+    .then((data) => data.json())
+    .then((post) => ({ data: post }));
