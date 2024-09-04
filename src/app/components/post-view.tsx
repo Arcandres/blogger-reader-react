c90ddc30079c7652f20cae18d3b7215fbdd1bf5f
Arcandres <arcandres@hotmail.com>
2024-09-03 22:07:12 -0500

    Unused icon removed

--- src/app/components/post-view.tsx
+++ src/app/components/post-view.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { TPost } from '../lib/types';
-import { ArrowBottomRightIcon, ReaderIcon } from '@radix-ui/react-icons';
+import { ArrowBottomRightIcon } from '@radix-ui/react-icons';
 
 type PostProp = {
   post: TPost;
