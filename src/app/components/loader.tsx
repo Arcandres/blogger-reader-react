662b3fcb421f1b52e187f2ce816a24f0b067dec7
Arcandres <arcandres@hotmail.com>
2025-01-11 15:34:47 -0500

    Adding preview blocks

--- src/app/components/loader.tsx
+++ src/app/components/loader.tsx
@@ -21,6 +21,8 @@ export default function Loader({ spinner = false }: { spinner?: boolean }) {
       <Skeleton />
       <Skeleton />
       <Skeleton />
+      <Skeleton />
+      <Skeleton />
     </div>
   );
 }
