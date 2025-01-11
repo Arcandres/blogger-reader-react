05c498366a12e4ebfcad60bbb3e6c73649f92189
Arcandres <arcandres@hotmail.com>
2025-01-11 15:33:59 -0500

    Changing width to preview for better adaptability.

--- src/app/components/skeleton.tsx
+++ src/app/components/skeleton.tsx
@@ -1,5 +1,5 @@
 export default function Skeleton() {
   return (
-    <div className='animate-pulse h-[150px] w-full rounded-lg bg-post/20' />
+    <div className='animate-pulse h-[150px] w-full max-w-md rounded-lg bg-post/20' />
   );
 }
